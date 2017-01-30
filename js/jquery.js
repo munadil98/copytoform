ca31eeb23c3107492ea9438c26ea58f4d32fb5d2
munadil98 <munadil98@gmail.com>
2017-01-30 10:52:58 +0600

    Add files via upload

--- js/jquery.js
+++ js/jquery.js
@@ -1,1 +1,107 @@
 
+$(document).ready(function() {
+
+	$(".btnName").click(function(){
+
+		// var	name=$(".table1 tr:nth-child(0) td:nth-child(1)").text();
+
+		var	name=$(".name").text();
+
+		$("#name").val(name);
+		// $(".p").text(name);
+
+	});	
+
+		$(".btnAge").click(function(){
+
+		// var	name=$(".table1 tr:nth-child(0) td:nth-child(1)").text();
+
+		var	age=$(".age").text();
+
+		$("#age").val(age);
+		// $(".p").text(name);
+
+	});	
+
+	$(".btnGender").click(function(){
+
+		// var	name=$(".table1 tr:nth-child(0) td:nth-child(1)").text();
+
+		var	gender=$(".gender").text();
+
+		$("#gender").val(gender);
+		// $(".p").text(name);
+
+	});	
+
+	$(".btnOccupation").click(function(){
+
+		// var	name=$(".table1 tr:nth-child(0) td:nth-child(1)").text();
+
+		var	occupation=$(".occupation").text();
+
+		$("#occupation").val(occupation);
+		// $(".p").text(name);
+
+	});	
+
+	$(".btnEmail").click(function(){
+
+		// var	name=$(".table1 tr:nth-child(0) td:nth-child(1)").text();
+
+		var	email=$(".email").text();
+
+		$("#email").val(email);
+		// $(".p").text(name);
+
+	});	
+
+	$(".btnPhone").click(function(){
+
+		// var	name=$(".table1 tr:nth-child(0) td:nth-child(1)").text();
+
+		var	phone=$(".phone").text();
+
+		$("#phone").val(phone);
+		// $(".p").text(name);
+
+	});	
+
+		$(".btnAll").click(function(){
+
+		// var	name=$(".table1 tr:nth-child(0) td:nth-child(1)").text();
+
+		var	name=$(".name").text();
+
+		$("#name").val(name);
+
+
+		var	age=$(".age").text();
+
+		$("#age").val(age);
+
+
+		var	gender=$(".gender").text();
+
+		$("#gender").val(gender);
+
+
+		var	occupation=$(".occupation").text();
+
+		$("#occupation").val(occupation);
+
+
+		var	email=$(".email").text();
+
+		$("#email").val(email);
+
+
+		var	phone=$(".phone").text();
+
+		$("#phone").val(phone);
+		// $(".p").text(name);
+
+	});	
+});
+
+
